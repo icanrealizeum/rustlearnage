85d574ffba617beed34046105d78f0f4302c0662
ActualizeInMaterial <none-because-deleted>
2016-06-27 04:16:13 +0300

    fixed guessing game

--- rust_book/guessing_game/src/main.rs
+++ rust_book/guessing_game/src/main.rs
@@ -1,47 +1,62 @@
 use std::io;
-use std::rand;
+extern crate rand;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = (rand::random::<u32>() % 100) + 1; // secret_number: i32
+//old:    let secret_number = (rand::random::<u32>() % 100) + 1; // secret_number: i32
     //alternate way to type hint from above ^ let x: u32 = rand::random();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
 //    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
-        let input = io::stdin().read_line()
+        let mut guess=String::new();
+        io::stdin().read_line(&mut guess)
             .ok()
             .expect("Failed to read line");
 
+//        let guess: u8 = guess.trim().parse().expect("not a number!");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         //    let input_num = "5".parse::<u32>(); // input_num: Option<u32>
         //    let input_num: Option<u32> = "5".parse(); // input_num: Option<u32>
-        let input_num: Option<u32> = input.trim().parse();
-        let num = match input_num {
+//old:        let input_num: Option<u32> = input.trim().parse();
+//        let input_num: Result<u32,_> = input.trim().parse();
+//        let Result<num:u32,_> = input.trim().parse();
+/*        let num = match input_num {
             Some(num) => num,
             None => {
                 println!("Please input a number!");
                 continue;
             }
-        };
+        };*/
 
-        println!("You guessed: {}", num);
 
-        match cmp(num, secret_number) {
+        println!("You guessed: {}", guess);
+
+//        match cmp(guess, secret_number) {
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
-                return;
+                //return;
+                break;
             },
         }
     }//loop
+    println!("normal exit.");
 }
 
-
+/*old
 fn cmp(a: u32, b: u32) -> Ordering {
     if a < b { Ordering::Less }
     else if a > b { Ordering::Greater }
     else { Ordering::Equal }
-}
+}*/
