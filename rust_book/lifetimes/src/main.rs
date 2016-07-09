0c5cb1d57bb88d7794ab677ad6e689989e3b7193
icanrealizeum <icanrealizeum@gmail.com>
2016-07-09 17:04:15 +0300

    fixed: like this I meant.

--- rust_book/lifetimes/src/main.rs
+++ rust_book/lifetimes/src/main.rs
@@ -3,12 +3,13 @@ struct Foo<'a> {
 }
 
 fn main() {
+    let _y=5;
     let x;                    // -+ x goes into scope
                               //  |
     {                         //  |
-        let y = &5;           // ---+ y goes into scope
+        let y = &_y;           // ---+ y goes into scope
         let f = Foo { x: y }; // ---+ f goes into scope
-        x = &f.x;             //  | | error here
+        x = f.x;             //  | | error here
     }                         // ---+ f and y go out of scope
                               //  |
     println!("{}", x);        //  |
