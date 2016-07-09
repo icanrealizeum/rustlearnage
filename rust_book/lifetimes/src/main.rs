39493bba6a1e621ca211128ccb712d2b3dd26f4b
icanrealizeum <icanrealizeum@gmail.com>
2016-07-09 17:03:00 +0300

    ok, fix this

---
+++ rust_book/lifetimes/src/main.rs
@@ -0,0 +1,16 @@
+struct Foo<'a> {
+    x: &'a i32,
+}
+
+fn main() {
+    let x;                    // -+ x goes into scope
+                              //  |
+    {                         //  |
+        let y = &5;           // ---+ y goes into scope
+        let f = Foo { x: y }; // ---+ f goes into scope
+        x = &f.x;             //  | | error here
+    }                         // ---+ f and y go out of scope
+                              //  |
+    println!("{}", x);        //  |
+}                             // -+ x goes out of scope
+
