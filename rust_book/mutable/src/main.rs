ce1477a018aa4febfec112e858e21c2e4f46b1ac
icanrealizeum <icanrealizeum@gmail.com>
2016-07-10 02:38:01 +0300

    here's immutable both

--- rust_book/mutable/src/main.rs
+++ rust_book/mutable/src/main.rs
@@ -10,6 +10,7 @@ fn main() {
     //let mut a=Point { x:1, y:2};
     let point: &mut Point = &mut Point { x: 0, y: 0 }; //ugleh - immutable point, mutable struct
     //let mut point = Point { x: 0, y: 0 }; //mutable point and struct - no thanks!
+    //let point: &Point = &Point { x: 0, y: 0 }; //immutable point and struct
 
     point.x = 5;
     //point = &mut a; //not allowed - GOOD!
