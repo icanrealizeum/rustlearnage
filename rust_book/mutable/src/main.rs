ad9874b963c2cb8bf87a4fb8eb643246dfa56834
icanrealizeum <icanrealizeum@gmail.com>
2016-07-10 02:34:18 +0300

    immutable binding -> mutable struct
    as opposed to mutable both

---
+++ rust_book/mutable/src/main.rs
@@ -0,0 +1,19 @@
+
+// src: https://doc.rust-lang.org/nightly/book/structs.html
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    //let mut a=Point { x:1, y:2};
+    let point: &mut Point = &mut Point { x: 0, y: 0 }; //ugleh - immutable point, mutable struct
+    //let mut point = Point { x: 0, y: 0 }; //mutable point and struct - no thanks!
+
+    point.x = 5;
+    //point = &mut a; //not allowed - GOOD!
+
+    println!("The point is at ({}, {})", point.x, point.y);
+}
+
