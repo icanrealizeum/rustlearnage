7eb4b5061055be396179a58bdf1dae0f233c7376
icanrealizeum <icanrealizeum@gmail.com>
2016-07-12 09:06:33 +0300

    instant Drop via _

---
+++ rustbyexample/14.3/drop/src/main.rs
@@ -0,0 +1,41 @@
+//src: https://rustbyexample.com/trait/drop.html
+struct Droppable {
+    name: &'static str,
+}
+
+// This trivial implementation of `drop` adds a print to console.
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("> Dropping {}", self.name);
+    }
+}
+
+fn main() {
+    let _ = Droppable { name: "a" }; //XXX: instantly dropped here
+
+    // block A
+    {
+        let _b = Droppable { name: "b" };
+
+        // block B
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+
+            println!("Exiting block B");
+        }
+        println!("Just exited block B");
+
+        println!("Exiting block A");
+    }
+    println!("Just exited block A");
+
+    // Variable can be manually dropped using the `drop` function
+    //drop(_a);
+    // TODO ^ Try commenting this line
+
+    println!("end of the main function");
+
+    // `_a` *won't* be `drop`ed again here, because it already has been
+    // (manually) `drop`ed
+}
