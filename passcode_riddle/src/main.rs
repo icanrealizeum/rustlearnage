ed5d008acf8b1fb4afbc901920117742cef34e96
icanrealizeum <icanrealizeum@gmail.com>
2016-07-21 16:43:40 +0300

    oops: for upper bound is exclusive!

--- passcode_riddle/src/main.rs
+++ passcode_riddle/src/main.rs
@@ -1,11 +1,13 @@
 // can you solve the passcode riddle https://www.youtube.com/watch?v=7Vd1dTBVbFg
-
+//uhm, why am I not getting 2 2 9?
 fn main() {
-    for x in 1..9 {
-        for y in 1..9 {
-            for z in 1..9 {
+    for x in 1..10 {
+        for y in 1..10 {
+            for z in 1..10 {
                 if (x<=y)&&(y<=z)&&(z != y)&&(x*y*z==36) {
                     println!("{} {} {} sum={}",x,y,z,x+y+z);
+                } else {
+//                    println!("{} {} {} rejected",x,y,z);
                 }
             }
         }
