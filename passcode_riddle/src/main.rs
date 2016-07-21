3f91440f115d52e484417a9ab707c4956fe0e6be
icanrealizeum <icanrealizeum@gmail.com>
2016-07-21 17:07:50 +0300

    use a constant! and type casting needed
    to avoid overflowing u8
    
    turn on backtrace again

--- passcode_riddle/src/main.rs
+++ passcode_riddle/src/main.rs
@@ -1,8 +1,12 @@
 // can you solve the passcode riddle https://www.youtube.com/watch?v=7Vd1dTBVbFg
 fn main() {
-    for x in 1..10 {
-        for y in 1..10 {
-            for z in 1..10 {
+    const MAX:u8 = 9;
+    //casting to different types! how nice is that ;-)
+    //println!("{}",1u8 as u16+1u16 as u16);
+//    let x:u16;
+    for x in 1..(1+MAX as u16) {
+        for y in 1..1u16+MAX as u16 {
+            for z in 1..1+(MAX as u16) {
                 if (x<=y)&&(y<=z)&&(z != y)&&(x*y*z==36) {
                     println!("{} {} {} sum={}",x,y,z,x+y+z);
                 } else {
@@ -11,4 +15,5 @@ fn main() {
             }
         }
     }
+    //println!("{}",x);
 }
