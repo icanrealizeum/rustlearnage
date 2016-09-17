92181c80f018768ee92c3d87917f5f5bfda90566
icanrealizeum <icanrealizeum@gmail.com>
2016-09-17 19:29:41 +0200

    min/max of vector elements via iter()

--- rust_sandbox/iter/src/main.rs
+++ rust_sandbox/iter/src/main.rs
@@ -2,10 +2,19 @@
 //src: https://github.com/rustomax/rust-iterators#digging-deeper
 
 fn main() {
-	for j in 1...3 {
-		for i in (1..11).map(|x| x +j) {
-			print!("{} ", i);
-		}
+    for j in 1...3 {
+        for i in (1..11).map(|x| x +j) {
+            print!("{} ", i);
+        }
         println!("");
-	}
+    }
+
+    let v = vec![3, 5, 0, -2, 3, 4, 1];
+//    let v:Vec<i32> = vec![];
+    let max = v.iter().max();
+    let min = v.iter().min();
+
+    println!("max = {:?}, min = {:?}", max, min);
+
+    // output: max = Some(5), min = Some(-2)
 }
